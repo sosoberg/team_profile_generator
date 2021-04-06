1f465402bc2b1ca240b8b639f31e7a613ec2baeb
Sam Oberg <samueloberg@hotmail.com>
2021-04-05 20:54:40 -0700

    working on making HTML dynamic

--- index.js
+++ index.js
@@ -22,29 +22,77 @@ inquirer
     },
     {
       type: 'input',
-      name: 'managerPhone',
-      message: 'Managers Work Phone:',
-    },
-    {
-      type: 'checkbox',
-      name: 'manageName',
-      message: 'Manager Name:',
-      choices: ['Engineer', 'Intern', 'Finish Team']
+      name: 'managerOffice',
+      message: 'Managers Office Number:',
     },
 
   ])
   .then((data) => {
 
-    const {  } = data;
+    const {  managerName, managerID, managerEmail, managerOffice } = data;
     
 
 
     const HTML = 
     `
+    <!DOCTYPE html>
 
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title></title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="./assets/reset.css">
+            <link rel="stylesheet" href="./assets/style.css">
+        </head>
+        <body>
+            <header>
+                <h1>My Team</h1>
+            </header>
+            <main>
+                <div>
+                    <h2 class"name">${ managerName }</h2>
+                        <p class="role">Manager</p>
+                        <p class="moreinfo">${ managerID }</p>
+                        <p class="moreinfo">${ managerEmail }</p>
+                        <p class="moreinfo">${ managerOffice }</p>
+                </div>
+                <div class"engineer">
+                    <h2 class"name"></h2>
+                        <p class="role"></p>
+                        <p class="moreinfo"></p>
+                        <p class="moreinfo"></p>
+                        <p class="moreinfo"></p>
+                </div>
+                <div class"intern">
+                    <h2 class"name"></h2>
+                        <p class="role"></p>
+                        <p class="moreinfo"></p>
+                        <p class="moreinfo"></p>
+                        <p class="moreinfo"></p>
+                </div>
+            </main>
+        </body>
+    </html>
     `
 
      fs.writeFile('index.html', HTML, (err) =>
        err ? console.log(err) : console.log('Website Created!')
      );
   });
+
+  // work on a append file to dynamically create this website
+
+  inquirer
+    .prompt([
+        {
+        type: 'checkbox',
+        name: 'teamSelector',
+        message: 'Manager Name:',
+        choices: ['Engineer', 'Intern', 'Finish Team']
+        },
+
+        
+    ])
